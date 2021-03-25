4cb3fdc1239e217f034c42d915de2b3aa1652e1e
abhishekbauriya <abhishekbauriya007@gmail.com>
2021-03-25 17:00:47 +0530

    Added Amazon HomePage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello Amzon Cloner</h1>
+
       {/* Header */}
+      <Header />
+
       {/* Home */}
+      <Home />
+      
     </div>
   );
 }
