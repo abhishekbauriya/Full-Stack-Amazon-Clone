997bcc51ee7f8216bf7f90168614dc829e932143
abhishekbauriya <abhishekbauriya007@gmail.com>
2021-03-27 22:57:06 +0530

    Added Payment Processing

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,9 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const stripe = require("stripe")("sk_test_51IZZD0SF04qc6K6A2Jq7IBl8BjSegnz1HwNGsME9UCbiMaUYDqscaHbcyCivo0T3b7WSAUB32rAZ91u9duySsob10067tdRuWy")
+const stripe = require("stripe")("sk_test_51IZZD0SF04qc6K6A2Jq7IBl8BjSegnz1HwNGsME9UCbiMaUYDqscaHbcyCivo0T3b7WSAUB32rAZ91u9duySsob10067tdRuWy");
 
 // API 
-
 
 // App Config
 const app = express();
@@ -19,7 +18,7 @@ app.get('/', (request, response) => response.status(200).send('hello world'))
 app.post('/payments/create', async (request, response) => {
     const total = request.query.total;
 
-    console.log('Payment Request Received BOOM!!! for this amount >>> ', total)
+    console.log('Payment Request Received BOOM!!! for this amount >>> ', total);
 
     const paymentIntent = await stripe.paymentIntents.create({
         amount: total, // subunits of the currency
@@ -29,11 +28,11 @@ app.post('/payments/create', async (request, response) => {
     // OK - Created
     response.status(201).send({
         clientSecret: paymentIntent.client_secret,
-    })
-})
+    });
+});
 
 // Listen command
-exports.api = functions.https.onRequest(app)
+exports.api = functions.https.onRequest(app);
 
 // Example endpoint
 // http://localhost:5001/clone-8db63/us-central1/api
--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-
 const instance = axios.create({
     baseURL: 'http://localhost:5001/clone-8db63/us-central1/api' 
     // THE API (cloud function) URL
